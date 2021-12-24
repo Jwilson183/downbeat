ec568ab59c084d08e5a5aa1d901304a28bc31270
Josiah Wilson <Jwilson183@sycamores.indstate.edu>
2021-12-23 21:41:03 -0500

    Test jump

--- Character.py
+++ Character.py
@@ -11,24 +11,21 @@ class Character(pygame.sprite.Sprite):
 		self.pos = vec(10, 0)
 		self.vel = vec(0,0)
 		self.acc = vec(0, 0)
-		#Variables
+		
+		#Physics
 		self.fric = fric
 		self.gravity = gravity
 		self.max_acceleration = max_acceleration
+
 		#Drawing Initial Position
 		self.surf = pygame.Surface((30, 30))
 		self.surf.fill((color))
 
 	def update(self):
 		self.get_input()
-		self.render()
+		self.move()
 		self.handle_collisions()
-
-		#Movement
-		if self.vel.x != 0:
-			self.acc.x += self.vel.x/abs(self.vel.x) * self.fric
-		self.vel += self.acc
-		self.pos += self.vel +0.5 * self.acc 	
+		self.render()
 
 	def get_input(self):
 		self.acc = vec(0,self.gravity)
@@ -38,19 +35,41 @@ class Character(pygame.sprite.Sprite):
 			self.acc.x = -self.max_acceleration
 		if self.should_move_right():
 			self.acc.x = self.max_acceleration
-
+		if self.should_move_up():
+			self.acc.y = -self.max_acceleration
+		if self.should_move_down():
+			self.acc.y = self.max_acceleration
+			
+		if self.should_jump():
+			self.jump()
+	
 	def should_move_left(self):
 		pass
-
 	def should_move_right(self):
 		pass
+	def should_move_up(self):
+		pass
+	def should_move_down(self):
+		pass
+
+	def should_jump(self):
+		pass
 	
-	def render(self):
-		self.rect = self.surf.get_rect(center = (self.pos.x, self.pos.y))
-		self.rect.midbottom = self.pos
+	#def jump(self):
+		#self.vel.y = -15
+
+	def move(self):
+		if self.vel.x != 0:
+			self.acc.x += self.vel.x/abs(self.vel.x) * self.fric
+		self.vel += self.acc
+		self.pos += self.vel + 0.5 * self.acc 	
 
 	def handle_collisions(self):
 		hits = pygame.sprite.spritecollide(self, sprites.platforms, False)
 		if hits:
 			self.pos.y = hits[0].rect.top + 1
 			self.vel.y = 0
+	
+	def render(self):
+		self.rect = self.surf.get_rect(center = (self.pos.x, self.pos.y))
+		self.rect.midbottom = self.pos
--- README.md
+++ README.md
@@ -3,4 +3,8 @@ So downbeat is a simple 2d platformer.
 There is a metronome ticking and whenever it hits the downbeat the gravity changes directions.
 The time signature and tempo might change with different levels.
 
-NOTE: Josiah is a poopypants. Also, make slippery and sticky surfaces in different levels. And jumpy surfaces.
+ Also, make slippery and sticky surfaces in different levels. And jumpy surfaces.
+
+ Maybe make a dash abillity
+	It might have to be on the beat in some way
+Pickups that give jumps and dashes
